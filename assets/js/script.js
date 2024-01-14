96a5fdf996d0115380d6dccb1a2d545dc4833294
Timea Farkas <tmeafarkas@yahoo.com>
2024-01-14 20:11:14 +0000

    write first JS scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,21 @@
 
 //Game Play
 
+//modal test
+let btn1 = document.getElementById('myBtn') 
+btn1.addEventListener('click', openModal)
+
+let modal = document.getElementById('myModal')
+let closeBtn = document.getElementsByClassName('close')[0]
+
+closeBtn.addEventListener('click', closeModal)
+modal.addEventListener('click', closeModal)
+
+function openModal() {
+    console.log('btn1 clicked')
+    modal.style.display = 'block';
+}
+function closeModal() {
+    console.log('modalClosed')
+    modal.style.display = 'none'
+}
