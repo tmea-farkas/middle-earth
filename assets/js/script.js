9965e961a8e2f36909f301800b69eab23d9e6d88
Timea Farkas <tmeafarkas@yahoo.com>
2024-01-16 17:59:48 +0000

    expand event listener to game entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // Home 
+
+// Modal added to display game rules when clicked
 let btn1 = document.getElementById('myBtn') 
 btn1.addEventListener('click', openModal)
 
@@ -8,6 +10,7 @@ let closeBtn = document.getElementsByClassName('close')[0]
 closeBtn.addEventListener('click', closeModal)
 modal.addEventListener('click', closeModal)
 
+//Two ways to close modal. On button click and if clicked outside of it
 function openModal() {
     console.log('btn1 clicked')
     modal.style.display = 'block';
@@ -17,14 +20,19 @@ function closeModal() {
     modal.style.display = 'none'
 }
 //Game Entry
-
 let advBtn = document.getElementById('adventure-btn')
-advBtn.addEventListener('click', toMoria)
+let closeLanding = document.getElementById('landing')
+let closeTitle = document.getElementById('title')
+advBtn.addEventListener('click', toMoria, closeLanding, closeTitle) //when button clicked landing diasppears and moria section appears
+
 
 function toMoria() {
     document.getElementById('moria').style.display = 'block';
-    console.log('At the gates')
+    document.getElementById('landing').style.display = 'none';
+    document.getElementById('title').style.display = 'none';
+    console.log('Landing Closed; At the gates')
 }
+
 
 
 //Game Play
